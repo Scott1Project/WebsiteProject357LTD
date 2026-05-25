3a1762f0b7cc95a4410264d40cc6a1d88a5733e8
Scott <scott.macdougall2@outlook.com>
2026-05-25 09:56:50 +0100

    Fixed newMember.php

--- newMember.php
+++ newMember.php
@@ -1,13 +1,6 @@
 <?php
 ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-session_start();
-
-if(!isset($_SESSION['myusername'])){
-    header("location:main_login.php");
-    exit;
-}
 
 $host="localhost";
 $username="in21006285";
@@ -17,25 +10,21 @@ $db_name="in21006285";
 $con = mysqli_connect($host, $username, $password, $db_name);
 
 if (mysqli_connect_errno()) {
-    die("Failed to connect to MySQL: " . mysqli_connect_error());
+    die("Failed to connect: " . mysqli_connect_error());
 }
 
-// Safely collect POST data
 $myusername = $_POST['User'] ?? '';
 $mypassword = $_POST['Pass'] ?? '';
 
-// Escape all data
 $myusername = mysqli_real_escape_string($con, $myusername);
 $mypassword = mysqli_real_escape_string($con, $mypassword);
 
-// Hash password
 $salt = "dontcrackmywebsite";
 $hash = crypt($mypassword, $salt);
 
-// INSERT query for login table
-$query = "INSERT INTO ACCOUNTS357 (username, password) VALUES ('$myusername', '$hash')";
+$query = "INSERT INTO ACCOUNTS357 (username, password)
+          VALUES ('$myusername', '$hash')";
 
-// Run query
 if (!mysqli_query($con, $query)) {
     die("Database error: " . mysqli_error($con));
 }
